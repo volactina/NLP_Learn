5c2e868fb64f71479a99d46ddcfcd2b687870375
volactina <10165101137@stu.ecnu.edu.cn>
2019-05-28 13:36:14 +0800

    Add files via upload

--- project1.py
+++ project1.py
@@ -1,7 +1,12 @@
 import os
 import re
 
-rootdir=r'C:\Users\lenovo\Desktop\NLP\IR_PROJECT\PROJECT 1\属性抽取\Project_Data'
+# text="e 3,m3"
+# ss=re.split('[ ]',text)
+# for s in ss:
+#     print(s)
+
+rootdir=r'C:\Users\叶文豪\Desktop\NLP\IR_PROJECT\PROJECT 1\IT\Project_Data'
 list = os.listdir(rootdir) #列出文件夹下所有的目录与文件
 for i in range(0,len(list)):
     path = os.path.join(rootdir, list[i])
@@ -11,19 +16,19 @@ for i in range(0,len(list)):
         # print(type(text))
         # print(text)
         # sentences=text.split('##。.,，、；;--:：  ')
-        sentences=text.split()
+        sentences=re.split('[ ，。．：、.]',text)
         for sentence in sentences:
             if ("start" in sentence or "end" in sentence or "---" in sentence):
                 continue
-            print(sentence)
+            if (("日" in sentence or "周岁" in sentence) and re.search('([0-9]|[一二三四五六七八九十零])+[日|周岁]',sentence)!=None):
+            # if ("日" in sentence or "周岁" in sentence):
+                print(sentence)
         print(sentences.__sizeof__())
-        matchObj=re.search(r'.?.?.?.?.?.?.?.?.?.?.?周岁.?.?.?.?.?.?.?.?',text,flags=0)
-        print(type(matchObj))
-        if matchObj:
-            print(matchObj.group())
+        # matchObj=re.search(r'.?.?.?.?.?.?.?.?.?.?.?周岁.?.?.?.?.?.?.?.?',text,flags=0)
+        # print(type(matchObj))
+        # if matchObj:
+        #     print(matchObj.group())
         # print(matchObj.group())
-        else:
-            print("Nothing found")
     break
 
 
